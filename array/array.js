e975e8856793a49e2fc1f4d1e9d0a4673bea7c70
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2023-04-23 12:52:23 +0530

    added basics of array

---
+++ array/array.js
@@ -0,0 +1,12 @@
+let car = ['swift','i20','aura','benz'];
+
+console.log(car);
+car.push('k10');
+
+
+console.log(car);
+console.log(car.length);
+
+for(var i=0;i<car.length;i++){
+    console.log(car[i]);
+}
