2caaf26e249e74a4af501afd5d952541fc473e41
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2023-04-23 13:06:58 +0530

    added simple file in map

---
+++ map/map.js
@@ -0,0 +1,13 @@
+const map = new Map();
+
+map.set(1,"Vishal");
+map.set(2,"indrajit");
+map.set(2,"sachin");
+map.set(2,"varalu");
+
+console.log(map);
+// map.clear();
+
+// console.log(map);
+
+console.log(map.get(1));
